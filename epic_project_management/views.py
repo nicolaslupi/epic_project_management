04c4b2b60c297064a3bb37cf1b8e1bc9924c57d1
lipinoelbreve <nicolupi.2@gmail.com>
2021-03-18 16:02:44 -0300

    Added Basic Tree Functionality. Captures Systems and Sub-systems

--- epic_project_management/views.py
+++ epic_project_management/views.py
@@ -1,6 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from data_loader.models import *
+
+def dependencies(root):
+    aux = dict()
+    sub_systems = System.objects.filter(depends_on = root.id)
+    for system in sub_systems:
+        aux[system] = dependencies(system)
+    return aux
+
+
 
 def home(request):
-    #return HttpResponse('homepage')
-    return render(request, 'homepage.html')
+    projects = Project.objects.all()
+    res = dict()
+    for project in projects:
+        assigned_systems = System.objects.filter(assigned_to_project = project.id, depends_on = None)
+        aux = dict()
+        for root in assigned_systems:
+            aux[root] = dependencies(root)
+        
+        res[project] = aux
+    
+    context = {
+        'data':res,
+    }
+
+    return render(request, 'homepage.html', context)
