a5a1f224a30ad4607e9bc13e9e6877b1394d8bc4
lipinoelbreve <nicolupi.2@gmail.com>
2021-03-19 16:43:31 -0300

    Extended tree functionality. Added style and dropdown lists

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
 console.log("1111",1111);
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+});
+}
