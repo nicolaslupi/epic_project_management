00f890057be91791f3e1bdaa7e0d57a0fa8c7622
lipinoelbreve <nicolupi.2@gmail.com>
2021-11-05 09:43:27 -0300

    Agrego Retiros a admin

--- data_loader/admin.py
+++ data_loader/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Item, System, Project, Person, Supplier, Compra
+from .models import Item, System, Project, Person, Supplier, Compra, Retiro
 # Register your models here.
 
-admin.site.register(Item)
 admin.site.register(Compra)
-#admin.site.register(ItemType)
+admin.site.register(Item)
+admin.site.register(Retiro)
 admin.site.register(System)
 admin.site.register(Project)
 admin.site.register(Person)
